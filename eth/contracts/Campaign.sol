4bdfea62508330cbe8a141984931493a6bddc957
Dishant Sharma <dishu5570@gmail.com>
2022-11-06 17:22:06 +0530

    completed tests and deployed the contract

--- eth/contracts/Campaign.sol
+++ eth/contracts/Campaign.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.4.17;
 contract CampaignFactory{
     address[] deployedCampaigns;
 
-    function createCampaign(uint minContri) public{
-        address newCampaign = new Campaign(minContri, msg.sender);
+    function createCampaign(uint minContri, string name) public{
+        address newCampaign = new Campaign(minContri, msg.sender, name);
         deployedCampaigns.push(newCampaign);
     }
 
@@ -28,16 +28,24 @@ contract Campaign{
     uint public minimumContribution;
     mapping(address=>bool) public approvers;
     uint public approversCount;
+    string CampainName;
 
     modifier restricted(){
         require(msg.sender==manager);
         _;
     }
 
-    function Campaign(uint minimumContri, address createrAddress) public{
+    function getManager() public view returns (address){
+        return manager;
+    }
+
+
+    function Campaign(uint minimumContri, address createrAddress, string name) public{
+        CampainName=name;
         manager = createrAddress;
         minimumContribution=minimumContri;
     }
+
 
     function contribute() public payable{
         require(msg.value>minimumContribution);
@@ -57,6 +65,10 @@ contract Campaign{
 
         requests.push(newRequest);
 
+    }
+
+    function getApproverAtAddress(address key) public view returns (bool){
+        return approvers[key];
     }
 
     function approveRequest(uint index) public {
---
+++ eth/deploy.js
@@ -0,0 +1,37 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs-extra')
+const Web3 = require('web3');
+const CampaignFactory = require('./build/CampaignFactory.json')
+
+const provider = new HDWalletProvider(
+  'easy bunker pass enroll sausage tribe green suit slow party damage cake',
+  // remember to change this to your own phrase!
+  'https://frosty-capable-card.ethereum-goerli.discover.quiknode.pro/57a47e849babccd5f99103c5cbb07ce77eb22178/'
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(CampaignFactory.interface))
+    .deploy({ data: CampaignFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+
+  const storeTheAddress = async () => {
+    try {
+      await fs.outputFile(`${__dirname}/deployedAddress.js`, result.options.address)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  storeTheAddress()
+
+  provider.engine.stop();
+};
+deploy();
